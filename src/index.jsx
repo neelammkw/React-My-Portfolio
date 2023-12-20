ee135eed7431b640cacee028ec873a9e014234cc
Neelam Makwana <hiyasoni5@gmail.com>
2023-12-20 21:27:59 +0530

    "git add"

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './sass/index.scss';
+import App from './App';
+import {BrowserRouter, HashRouter} from 'react-router-dom'
+import { HashRouter as Router} from 'react-router-dom';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BrowserRouter >
+//   <HashRouter basename="/">
+//     <App />
+//     </HashRouter>
+//   </BrowserRouter>
+// );
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
